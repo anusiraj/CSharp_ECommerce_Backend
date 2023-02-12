57606ebf95e9f823a9433bc3ce87274e5d00cf2f
Anu Siraj <anusiraj007@gmail.com>
2023-02-12 17:36:05 +0200

    Code refactoring

--- Design/Models/Product.cs
+++ Design/Models/Product.cs
@@ -6,16 +6,16 @@ public class Product
 {
   public int ID { get; init; }
   public string Name { get; set; }
-  public string Category { get; set; }
+  public int CategoryID { get; set; }
   public int Price { get; set; }
   public string Descritption { get; set; }
   public string[] Images { get; set; }
 
-  public Product(int id, string name, string category, int price, string description, string[] images)
+  public Product(int id, string name, int categoryId, int price, string description, string[] images)
   {
     ID = id;
     Name = name;
-    Category = category;
+    CategoryID = categoryId;
     Price = price;
     Descritption = description;
     Images = images;
@@ -23,7 +23,7 @@ public class Product
 
   public override string ToString()
   {
-    return "Product ID:" + ID + " " + "Name" + Name + " " + "Category" + Category + " " + "Price" + Price + "Description" + Descritption + " " +
+    return "Product ID:" + ID + " " + "Name" + Name + " " + "Category" + CategoryID + " " + "Price" + Price + "Description" + Descritption + " " +
     " " + "Image" + Images[0] + " ";
   }
 }
